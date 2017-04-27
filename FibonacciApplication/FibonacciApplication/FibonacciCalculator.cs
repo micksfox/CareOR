d4b075e40dbdd2186b59a466afcd8b85c258c26e
micksfox <micsfox@gmail.com>
2017-04-27 12:34:36 -0400

    Add files via upload

---
+++ FibonacciApplication/FibonacciApplication/FibonacciCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FibonacciApplication
+{
+    public static class FibonacciCalculator
+    {
+        /// Calculate the Fibonacci series position value.
+        ///
+        /// Calculates the Finonacci series position value using a standard
+        /// calculation with linear run time and constant space.
+        /// 
+        /// @param  Position in the Fibonacci series.
+        /// @return Calculated value.
+        /// 
+        public static int CalculateFibonacci(int position)
+        {
+            int F_N = 0;    // Current value.
+            int F_N1 = 1;   // Last value.
+            int F_N2 = 1;   // Second to last value.
+
+            for (int index = 3; index <= position; index++)
+            {
+                F_N = F_N1 + F_N2;
+
+                F_N1 = F_N2;
+                F_N2 = F_N;
+            }
+
+            return F_N;
+        }
+    }
+}
---
+++ FibonacciApplication/FibonacciApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FibonacciApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Fibonacci Calculator!");
+            Console.WriteLine();
+
+            // Displays the Fibonacci series value for position 10 & 40 to the console.
+            WriteFibonacciNumber(10);
+            WriteFibonacciNumber(40);
+
+            Console.ReadLine();
+        }
+
+        /// Write the Fibonacci series value for given position to the console.
+        ///
+        /// @param  Position in the Fibonacci series.
+        /// 
+        static void WriteFibonacciNumber(int position)
+        {
+            int value = FibonacciCalculator.CalculateFibonacci(position);
+            Console.WriteLine(String.Format("The value at position {0} is {1}", position, value));
+        }
+    }
+}
